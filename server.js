517fb7914409d5975402c02be36752a10e8e1f1f
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-13 21:00:07 -0500

    complete server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller");
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on: ${PORT}`);
+});
