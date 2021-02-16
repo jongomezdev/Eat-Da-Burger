9671b2c2ed036adbc854034f4d49b631dde130bd
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-15 20:03:06 -0500

    debug issues

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -6,3 +6,16 @@ $("#create-burger").submit(() => {
     $.post("/api/burgers", data, () => location.reload());
   }
 });
+
+$(".devour-btn").click(function () {
+  const id = $(this).attr("data-id");
+  $.ajax(`/api/burgers/${id}`, {
+    type: "PATCH",
+  }).then(() => location.reload());
+});
+
+$("#clear-btn").click(() => {
+  $.ajax("/api/burgers", {
+    type: "DELETE",
+  }).then(() => location.reload());
+});
